58b877af356a5446e3a81c75c5c90ddbadad38ed
huanruiz <huanruiz@foxmail.com>
2023-05-27 19:54:16 +0800

    114

--- algorithm/java/q114_flatten_binary_tree_to_linked_list/FlattenBinaryTreeToLinkedList.java
+++ algorithm/java/q114_flatten_binary_tree_to_linked_list/FlattenBinaryTreeToLinkedList.java
@@ -1,3 +1,35 @@
 package q114_flatten_binary_tree_to_linked_list;
 
 public class FlattenBinaryTreeToLinkedList {}
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void flatten(TreeNode root) {
+        if (root == null) {
+           return; 
+        }
+        flatten(root.left);
+        flatten(root.right);
+        TreeNode tmp = root.right;
+        root.right = root.left;
+        root.left = null;
+        TreeNode node = root;
+        while (node.right != null) {
+            node = node.right;
+        }
+        node.right = tmp;
+    }
+}
